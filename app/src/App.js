d059c1fb57154674087c4b29efd569f5c67e5853
claytoncritchfield <clayton.critchfield@gmail.com>
2023-06-29 10:49:30 -0400

    created heading component, imported to app.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,14 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Heading from './routes/Heading.js';
 import Footer from './routes/Footer.js';
 import './index.css';
 
 
 export default function App() {
   return (
-  
+    <Heading />
     <Footer />
   )
 }
