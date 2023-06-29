02ec516ff033e6f1face9aef54816f2f19a0ad1f
claytoncritchfield <clayton.critchfield@gmail.com>
2023-06-29 10:25:46 -0400

    added bg color to body, and color to headings

--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import Footer from './routes/Footer.js';
 import App from './App';
 
 
