c43460c30bdc5841e06bc19b84fc90837f32302b
claytoncritchfield <clayton.critchfield@gmail.com>
2023-06-29 11:09:22 -0400

    added bootstrap class text-center to Footer component

--- app/src/routes/Footer.js
+++ app/src/routes/Footer.js
@@ -1,11 +1,11 @@
 export default function Footer () {
     return (
-        <>
+        <div className="text-center">
         <h2>Address : </h2>
         <p>348 E Main St <br></br> 
         Lexington, KY<br></br>
         40507
         </p>
-        </>
+        </div>
     );
 }
