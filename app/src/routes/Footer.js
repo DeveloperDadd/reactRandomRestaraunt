99112b280326848197db74e4c0f75243c7ad63bc
claytoncritchfield <clayton.critchfield@gmail.com>
2023-06-29 11:22:45 -0400

    added some more styling for the footer

--- app/src/routes/Footer.js
+++ app/src/routes/Footer.js
@@ -1,11 +1,30 @@
 export default function Footer () {
     return (
-        <div className="text-center">
+        <>
+        <footer className="text-center container-fluid">
+        <div className="row .d-flex justify-content-center">
+        
+        <div className="col-6">
         <h2>Address : </h2>
         <p>348 E Main St <br></br> 
         Lexington, KY<br></br>
         40507
         </p>
         </div>
+        
+        <div className="col-6">
+        <h2>Hours of operation</h2>
+        <p className="text-center">Monday through Friday : 11:00am - 8:00pm</p>
+        <p className="text-center">Saturday : 10:00am - 9:00pm</p>
+        </div>
+        
+        
+
+
+        </div>
+        
+        
+        </footer>
+        </>
     );
 }
