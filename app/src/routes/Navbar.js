a7aabcfcdea1027321d35c540832338b33fa66ea
claytoncritchfield <clayton.critchfield@gmail.com>
2023-06-29 20:17:57 -0400

    got the API to work finally!

--- app/src/routes/Navbar.js
+++ app/src/routes/Navbar.js
@@ -2,30 +2,36 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function Navbar () {
-    const [category, setCategory] = useState([category]);
+    const [menu, setMenu] = useState([]);
 
     const menus = ['Appetizers','Breakfast','Lunch','Dinner','Drinks']
 
-function handleClick() {
     useEffect(() => {
         axios.get('https://www.jsonkeeper.com/b/MDXW')
             .then(res => {
-                setCategory(res.data);
+                setMenu(res.data);
             })
-    }, []); }
-    
+    }, []); 
+
     return (
     <>
         <nav class="container-sm">
             <ul class="container-fluid .d-flex justify-content-center">
                 {menus.map((menu) => (
-                    <li key={menu}><button onClick={handleClick}>{menu}</button></li>
+                    <li key={menu}><button onClick={useEffect}>{menu}</button></li>
                 ))}
             </ul>
         </nav>
 
         <div className="container-fluid">
-
+             {menu.map(item => (
+                <div key={item.id}>
+                    <h3>{item.title}</h3>
+                    <h6>{item.description}</h6>
+                    <h6>{item.category}</h6>
+                    <h6>{item.price}</h6>
+                </div>
+             ))}       
 
         </div>
     </>
