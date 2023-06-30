2b7ddba14668d34bef8ee787c655f5879f4d76f5
claytoncritchfield <clayton.critchfield@gmail.com>
2023-06-29 22:24:39 -0400

    fixed API call, filtered menu variables, hard coded buttons with some functionality

--- app/src/routes/Navbar.js
+++ app/src/routes/Navbar.js
@@ -2,29 +2,47 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function Navbar () {
-    const [menu, setMenu] = useState([]);
+    const [menuState, setMenuState] = useState([]);
 
-    const menus = ['Appetizers','Breakfast','Lunch','Dinner','Drinks']
+    const menus = ['Appetizer','Breakfast','Lunch','Dinner','Drink']
 
     useEffect(() => {
+        async function getData() {}
         axios.get('https://www.jsonkeeper.com/b/MDXW')
             .then(res => {
-                setMenu(res.data);
+                setMenuState(res.data);
             })
     }, []); 
+
+    const appetizers = menuState.filter((item) => item.category === 'Appetizer');
+    const breakfast = menuState.filter((item) => item.category === 'Breakfast');
+    const lunch = menuState.filter((item) => item.category === 'Lunch');
+    const dinner = menuState.filter((item) => item.category === 'Dinner');
+    const drinks = menuState.filter((item) => item.category === 'Drink');
+
+    //button is clicked at top
+    //setMenu = 
+    //changes menu to whatever button is clicked
+
+
+    function handleClick(category) {
+        setMenuState(category)
+    }
 
     return (
     <>
         <nav class="container-sm">
             <ul class="container-fluid .d-flex justify-content-center">
-                {menus.map((menu) => (
-                    <li key={menu}><button onClick={useEffect}>{menu}</button></li>
-                ))}
+                <li><button onClick={handleClick(appetizers)}>{menus[0]}</button></li>
+                <li><button onClick={handleClick}>{menus[1]}</button></li>
+                <li><button onClick={handleClick}>{menus[2]}</button></li>
+                <li><button onClick={handleClick}>{menus[3]}</button></li>
+                <li><button onClick={handleClick}>{menus[4]}</button></li>
             </ul>
         </nav>
 
         <div className="container-fluid">
-             {menu.map(item => (
+             {menuState.map(item => (
                 <div key={item.id}>
                     <h3>{item.title}</h3>
                     <h6>{item.description}</h6>
